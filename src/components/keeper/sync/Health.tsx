4f17a7fd7d775d67f3fdab290bce4087085f8a6a
cam-ball <hi@camball.me>
2024-03-31 23:03:18 -0400

    Add `Health` component

---
+++ src/components/keeper/sync/Health.tsx
@@ -0,0 +1,55 @@
+import { useState, ChangeEvent } from "react";
+
+type HealthProps = {
+  syncValue: number;
+  handleSyncChange: (value: number) => void;
+};
+const Health = ({ syncValue, handleSyncChange }: HealthProps) => {
+  const [inputValue, setInputValue] = useState<number>(1);
+
+  const addSync = () => {
+    handleSyncChange(Math.min(100, syncValue + inputValue));
+  };
+
+  const reduceSync = () => {
+    handleSyncChange(Math.max(0, syncValue - inputValue));
+  };
+
+  return (
+    <>
+      <div className="mt-4 text-7xl">
+        <h4>{syncValue}</h4>
+      </div>
+      <div className="mt-4 flex flex-row justify-around">
+        <button
+          onClick={reduceSync}
+          className="border border-2 border-red-700 px-4 text-3xl text-red-700"
+        >
+          -
+        </button>
+        <input
+          type="number"
+          min="0"
+          max="100"
+          step="1"
+          className="w-24 text-center text-3xl"
+          value={inputValue}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            const v = Number(e.target.value);
+            if (!isNaN(v)) {
+              setInputValue(v);
+            }
+          }}
+        />
+        <button
+          onClick={addSync}
+          className="border-2 border-green-700 px-4 text-3xl text-green-700"
+        >
+          +
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Health;
--- src/components/keeper/sync/index.tsx
+++ src/components/keeper/sync/index.tsx
@@ -1,54 +1,19 @@
-import { useState, ChangeEvent } from "react";
+import { useState } from "react";
+import Health from "./Health";
 
 const Sync = () => {
-  const [previousSync, setPreviousSync] = useState<number>(100);
+  const [_previousSync, setPreviousSync] = useState<number>(100);
   const [syncValue, setSyncValue] = useState<number>(100);
-  const [inputValue, setInputValue] = useState<number>(1);
 
-  const addSync = () => {
+  const handleSyncChange = (newSync: number) => {
     setPreviousSync(syncValue);
-    setSyncValue(Math.min(100, syncValue + inputValue));
-  };
-
-  const reduceSync = () => {
-    setPreviousSync(syncValue);
-    setSyncValue(Math.max(0, syncValue - inputValue));
+    setSyncValue(newSync);
   };
 
   return (
     <div className="min-h-52 basis-1/3 p-8 text-center">
       <h3 className="font-mono">// S–Y–N–C //</h3>
-      <div className="mt-4 text-7xl">
-        <h4>{syncValue}</h4>
-      </div>
-      <div className="mt-4 flex flex-row justify-around">
-        <button
-          onClick={reduceSync}
-          className="border border-2 border-red-700 px-4 text-3xl text-red-700"
-        >
-          -
-        </button>
-        <input
-          type="number"
-          min="0"
-          max="100"
-          step="1"
-          className="w-24 text-center text-3xl"
-          value={inputValue}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            const v = Number(e.target.value);
-            if (!isNaN(v)) {
-              setInputValue(v);
-            }
-          }}
-        />
-        <button
-          onClick={addSync}
-          className="border-2 border-green-700 px-4 text-3xl text-green-700"
-        >
-          +
-        </button>
-      </div>
+      <Health syncValue={syncValue} handleSyncChange={handleSyncChange} />
     </div>
   );
 };
